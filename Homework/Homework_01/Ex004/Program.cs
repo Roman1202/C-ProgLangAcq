8926a34f60a7d378288527bdda94be8fc4ae7d2a
Roman <roman.panchenko.1999@mail.ru>
2023-02-25 12:35:19 +0600

    fourth ex is done

--- Homework/Homework_01/Ex004/Program.cs
+++ Homework/Homework_01/Ex004/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int counter = 2;
+
+while (counter <= N)
+{
+Console.Write(counter + " ");
+counter = counter + 2;
+}
