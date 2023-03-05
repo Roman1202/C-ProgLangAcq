5a5709cc876bb633119e823d156d0448cd0543c5
Roman <roman.panchenko.1999@mail.ru>
2023-03-05 16:57:02 +0600

    Added the homework03

---
+++ Homework/Homework_03/Ex008/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.Write("Enter a number: ");
+string number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Your number: {number} - palindrome.");
+  }
+  else Console.WriteLine($"Your number: {number} - isn't a palindrome.");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Enter the correct number");
