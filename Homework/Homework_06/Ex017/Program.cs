7e65f0c9448cbdabbbd09d47c74fbf1a2d418d75
Roman <roman.panchenko.1999@mail.ru>
2023-03-15 09:12:04 +0600

    Added Homework_06

---
+++ Homework/Homework_06/Ex017/Program.cs
@@ -0,0 +1,31 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь(41)
+int Accept(string message){
+    System.Console.Write(message); 
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int[]Array(int length){ //заполняет массив данными с терминала
+    int[]array=new int[length];
+    for(int i=0; i<array.Length; i++){
+        array[i]=Accept($"Введите {i+1}-й элемент ");
+    }
+    return array;
+}
+void Print(int[] array){//определяет для каждого индекса свой элемент
+    for(int i=0; i<array.Length; i++){
+        System.Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+int Count(int[] array){
+    int count = 0;
+    for(int i=0; i< array.Length; i++){
+        if(array[i]>0)count++;
+    }
+    return count;
+}
+int length= Accept("Введите количество элементов ");
+int[]array;
+array=Array(length);
+Print(array);
+System.Console.WriteLine($"Количество чисел больше нуля равно- {Count(array)}");
---
+++ Homework/Homework_06/Ex018/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, 
+//y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
+double Accept(string message){
+    System.Console.Write(message); 
+    string readInput = System.Console.ReadLine();
+    double result = Convert.ToDouble(readInput);
+    return result;
+}
+double CoordX (double b1, double b2, double k1, double k2){
+    double x = (-b2 + b1)/(-k1 + k2); //поиск координаты х
+    return x;
+}
+double CoordY (double x, double b2, double k2){ 
+    double y = k2 * x + b2; //поиск координаты У
+    return y;
+}
+double b1 = Accept("введите значение b1 ");
+double k1 = Accept("введите значение k1 ");
+double k2 = Accept("введите значение k2 ");
+double b2 = Accept("введите значение b2 ");
+double x = CoordX (b1,b2,k1,k2);
+double y = CoordY (x,b2,k2);
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
